1fdb4a1130238fb60fc494f0fc7b1a101c3b509b
vinuuVR <vinusharashmika87@gmail.com>
2026-03-19 05:24:46 +0530

    Updated the android gradle file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.example.cleansl_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -49,6 +49,10 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+
+    aaptOptions {
+        noCompress 'tflite'
     }
 }
 
